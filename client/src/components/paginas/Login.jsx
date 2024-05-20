3b08dc11b5c1d6ff4f7294381e5ff0cf0c943d13
DaSViM <david.villar@fiuni.edu.py>
2024-05-20 08:52:39 -0400

    Login page

---
+++ client/src/components/paginas/Login.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Login = () => {
+
+    return (
+        <div className="container-auth">
+            <h2>Login</h2>
+
+            <form>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="E-mail"
+                />
+                <input
+                    name="pass"
+                    type="password"
+                    placeholder="Password"
+                />
+
+                <div className="container-buttons">
+                    <button type="submit">Log In</button>
+                    <button type="button"> Google </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
